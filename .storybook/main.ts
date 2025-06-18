da7976af04336635406d8f139c3471c3b7879a8a
THEMANSI <49090219+Mansishrivastava@users.noreply.github.com>
2025-06-18 14:58:24 +0530

    Add files via upload

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,1 +1,30 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ],
+  viteFinal: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'next/navigation': '/src/mocks/nextRouter.tsx',
+    };
+    return config;
+  },
+};
+export default config;
